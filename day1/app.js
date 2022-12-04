82c8bc6af71a4b9e791ae128ec515ae870690fbf
Arun Khanal <59117742+arunkhanal7@users.noreply.github.com>
2022-12-04 20:23:03 +0545

    Add files via upload

---
+++ day1/app.js
@@ -0,0 +1,51 @@
+//declare a variable 
+// var myName="Arun"
+
+// let ourname="freecode"
+
+// const pi=3.14 //const cannot be changed 
+
+// let a=7;
+// document.write(a+a);
+
+// let str1='arun';
+// let str2='khanal';
+//  console.log(str1+str2);
+
+// //double quoted string: by using escape character(\")
+let str3="Ram said \"he is not free\" tomorrow morning."
+// let str4='Ram said "he is not free" tomorrow morning.';
+// str3+=' So do I ' //string concatination can be done also by this way
+// document.write(str3);
+// document.write(str4);
+
+//length of the string
+// str3length=str3.length;
+// document.write(str3length);
+
+// arryas 
+// var arr=[1,2,'arun',3,4,5];
+// document.write(arr);
+// document.write(arr[2]);
+
+//multi dimensional arrays
+// var myarray=[[1,2,3],[4,5,6],[7,8,9,[[10,11,12],13,14]]]
+// var myData=myarray[2][1];
+// document.write(myData);
+
+//array push method  add element on last
+// var myarr=["rabin","koirala"];
+// myarr.push(["sanjay","susham"]);
+// document.write(myarr);
+// myarr.pop();//remove the last item from the array and shift() function works like pop function but it remove the first element
+// document.write(myarr);
+// myarr.unshift(['arun','dai']); //unshift add element in the beginning 
+// document.write(myarr);
+
+//function
+function average(a,b) {
+    return (a+b)/2;
+}
+c=average(10,14);
+document.write(c);
+
